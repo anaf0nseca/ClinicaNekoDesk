c001fe87c0816da781165eb1fa34413e0d58c2aa
ANA CAROLINA DE SOUZA FONSECA <ana.csfonseca5@senacsp.edu.br>
2024-11-22 11:35:54 -0300

    .

--- ClinicaNekoLib/obj/Debug/net8.0/ClinicaNekoLib.AssemblyInfo.cs
+++ ClinicaNekoLib/obj/Debug/net8.0/ClinicaNekoLib.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicaNekoLib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a71efb2bcbf01e09536cf703d5b6cb4ced8beed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicaNekoLib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicaNekoLib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
